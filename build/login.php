14b4805e173b1f95eb5c7c9df3323ce6938dbc74
Edhyjox <edhyjox@gmail.com>
2021-04-11 12:39:40 +0200

    login page and css

--- build/login.php
+++ build/login.php
@@ -4,21 +4,18 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="container">
-    <div class="form-wrapper">
 
-        <form method="post">
-            <label for="username">
-                <input id="username" name="username" type="text" required>
-            </label>
+<form method="post">
+    <label for="username">
+        <input id="username" name="username" type="text" required>
+    </label>
 
-            <label for="password">
-                <input id="password" name="password" type="password" required>
-            </label>
+    <label for="password">
+        <input id="password" name="password" type="password" required>
+    </label>
 
-            <input type="submit" value="Login">
-        </form>
-    </div>
-</div>
+    <input type="submit" value="Login">
+</form>
+
 </body>
 </html>
