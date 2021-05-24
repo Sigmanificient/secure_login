f5fdbcd238107790967e81689b3e4d1040f210a2
Sigmanificient <edhyjox@gmail.com>
2021-05-24 23:46:33 +0200

    Moving login.php to views & remove template code

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,11 +1,4 @@
-<html lang="fr">
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<form method="post" action="includes/process_login.php">
+<form method="post" action="../public/includes/process_login.php">
     <label for="username">
         <input id="username" name="username" type="text" required>
     </label>
@@ -16,6 +9,3 @@
 
     <input type="submit" value="Login">
 </form>
-
-</body>
-</html>
