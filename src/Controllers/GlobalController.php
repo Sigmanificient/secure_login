8808be1fe5453a40d5bf080da0550a2c2d9abc20
Sigmanificient <edhyjox@gmail.com>
2021-05-24 23:40:47 +0200

    Added missing connect button

---
+++ src/Controllers/GlobalController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once ROOT . '/Controllers/Controller.php';
+
+class GlobalController extends Controller
+{
+
+    public function default()
+    {
+        $this::render('landing');
+    }
+}
