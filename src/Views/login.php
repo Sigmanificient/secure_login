bf29d4b7d50225bfce19dd06ffe76c76f102cc5d
Sigmanificient <edhyjox@gmail.com>
2021-06-03 09:53:21 +0200

    Minimal login

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,3 +1,7 @@
+<?php
+/* @var array $params */
+?>
+
 <form method="post" action="<?= SITE . '/User/process_login' ?> ">
     <label for="username">
         <input id="username" name="username" type="text" required>
@@ -9,3 +13,22 @@
 
     <input type="submit" value="Login">
 </form>
+
+<?php if ($params['error']): ?>
+    <p>
+        <?php
+        switch ($params['error']) {
+            case 'empty':
+                echo 'Please enter your username and password';
+                break;
+
+            case 'invalid':
+                echo 'Invalid username or password';
+                break;
+
+            default:
+                echo 'Unknown error';
+        }
+        ?>
+    </p>
+<?php endif; ?>
