33b01db7603c28a6f8d2157f72bbfc89c7eb356a
Sigmanificient <edhyjox@gmail.com>
2021-06-03 10:35:38 +0200

    script for a dummy user

---
+++ db/add_dummy_user.sql
@@ -0,0 +1,3 @@
+insert into secure_login.users(uid, authentication_string)
+values ('demo',
+        'ae2f8cadff9b433f03e0eee90181d355307480e14a0e7a7a1cf626826165216a0de3c1b620344fcf023be5f6cbdb8d2b486d3030d73016d2a180b82b71d9822b');
