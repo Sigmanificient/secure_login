2ed00574452b6a1f399d35e64529e77ea52363f3
Yohann Boniface <edhyjox@gmail.com>
2021-06-03 13:55:03 +0200

    Update DBconnexion.php

--- src/core/DBconnexion.php
+++ src/core/DBconnexion.php
@@ -1,19 +1,18 @@
 <?php
 
-require_once ROOT . '/Core/Singleton.php';
+require_once ROOT . '/core/Singleton.php';
 
 class DBConnexion extends Singleton
 {
-    private $__conf_path = ROOT . '/Core/db.ini';
 
-    private $_host = "localhost";
+    private $_host = 'localhost';
     /* localhost or DBServer IP. If you using a remote server, make sure to only accept critical ip and not 0.0.0.0 */
 
-    private $_dbname = "secure_login";
-    private $_dbuser = "secure_login";
+    private $_dbname = 'secure_login';
+    private $_dbuser = 'secure_login';
     /* Make sure that the application have it own user and minimal permission on the database */
 
-    private $_dbpassword = "SfbDE24GMA_ikXa-3EOq-nwNJA_NwEm1";
+    private $_dbpassword = 'SfbDE24GMA_ikXa-3EOq-nwNJA_NwEm1';
     /*
     Your DB password should always be 16+ Characters long, having lowercase, uppercase, digits, special symbol.
     If you use UTF-8 in your password, make sure it wont cause issue in the production environment.
@@ -26,7 +25,7 @@ class DBConnexion extends Singleton
     {
         try {
             $this->_conn = new PDO(
-                'mysql:host=' . $this->_host . ";dbname=" . $this->_dbname . ';charset=utf8',
+                'mysql:host=' . $this->_host . ';dbname=' . $this->_dbname . ';charset=utf8',
                 $this->_dbuser, $this->_dbpassword
             );
 
